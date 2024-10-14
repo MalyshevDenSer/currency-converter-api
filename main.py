3c20ed5180e83394c8b169065f9016f89c5d2062
Denis <maldenser@gmail.com>
2024-10-14 18:23:38 +0300

    currency code validator

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import logging
-from typing import Annotated, Literal
-
+from typing import Annotated
 from fastapi import FastAPI, Query
-from pydantic import BaseModel, Field
 from moneyed import list_all_currencies
+from pydantic import BaseModel, field_validator
 
 app = FastAPI()
 
@@ -11,11 +9,21 @@ currency_codes = [currency.code for currency in list_all_currencies()]
 
 
 class FilterParams(BaseModel):
-    from_currency: Literal[tuple(currency_codes)]
-    to_currency: Literal[tuple(currency_codes)]
+    from_currency: str = Query(alias='from')
+    to_currency: str = Query(alias='to')
     value: int
 
+    @field_validator('from_currency', 'to_currency')
+    def validate_currency(cls, value: str):
+        if value not in currency_codes:
+            raise ValueError(
+                f"The value must be one of the valid currency codes defined in "
+                f"ISO 4217 Currencies (except North Korean Won): {', '.join(currency_codes)}"
+            )
+        return value
 
-@app.get("/rates")
-async def read_items(filter_query: Annotated[FilterParams, Query()]):
-    logging.info(filter_query)
+
+@app.get("/api/rates")
+async def read_items(filtered_query: Annotated[FilterParams, Query()]):
+    print(filtered_query)
+
